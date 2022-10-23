797201de17f53306dea311ea875b3e250c5ef08b
AliBarkinKara07 <111884407+AliBarkinKara07@users.noreply.github.com>
2022-10-23 17:50:04 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//try
+//{
+//    Console.WriteLine("Bir sayı giriniz:");
+//    int sayi = Convert.ToInt32(Console.ReadLine());
+//    Console.WriteLine("Girmiş Olduğunuz Sayı" + sayi);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine("Hata:" + ex.Message.ToString());
+
+//}
+
+try
+{
+    // int a = int.Parse(null);
+    int a = int.Parse("test");
+}
+catch (ArgumentNullException exe)
+{
+    Console.WriteLine("Boş Değer Girdiniz");
+    Console.WriteLine(exe);
+}
+
+catch(FormatException exe)
+{
+    Console.WriteLine("Veri Tipi Uygun Değil");
+    Console.WriteLine(exe);
+}
